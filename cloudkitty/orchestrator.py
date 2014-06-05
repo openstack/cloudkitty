ade198e620855004d610477dba9bc4c4478827e2
Gauvain Pocentek <gauvain.pocentek@objectif-libre.com>
2014-06-05 17:26:33 +0200

    Set copyright/license information in .py files
    Also remove unneeded shebangs and encoding information.
    
    Change-Id: I619f54d1c92653939f668a95456c23b2c70edb9a

--- cloudkitty/orchestrator.py
+++ cloudkitty/orchestrator.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-File: orchestrator.py
-Author: Stephane Albert
-Email: stephane.albert@objectif-libre.com
-Github: http://github.com/objectiflibre
-Description: CloudKitty, Orchestrator
-"""
+# Copyright 2014 Objectif Libre
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Stéphane Albert
+#
 from datetime import datetime
 import sys
 import time
