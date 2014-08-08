5765daf6c22ec61b958620c0cd811c8cda9c0cd8
Stéphane Albert <stephane.albert@objectif-libre.com>
2014-08-08 14:17:38 +0200

    Added i18n support
    Change-Id: I586b0e604951b158ef1f6f0adbc31a61489f0047

---
+++ cloudkitty/i18n.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Objectif Libre
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Stéphane Albert
+#
+from oslo import i18n  # noqa
+
+_translators = i18n.TranslatorFactory(domain='cloudkitty')
+i18n.enable_lazy()
+
+_ = _translators.primary
+_LI = _translators.log_info
+_LW = _translators.log_warning
+_LE = _translators.log_error
+_LC = _translators.log_critical
