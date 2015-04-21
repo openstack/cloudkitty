b81d800532ccaf67843c17fc176cb7cab2b4e5c5
Stéphane Albert <stephane.albert@objectif-libre.com>
2015-04-21 18:26:31 +0200

    Fixed regression in RatingProcessorBase: nodata missing
    During a previous rebase of a branch the nodata part of
    RatingProcessorBase was erroneously removed.
    
    Change-Id: Idc1afcf5e734481b3fb461020ddbd4207e8921b9

--- cloudkitty/rating/__init__.py
+++ cloudkitty/rating/__init__.py
@@ -106,6 +106,14 @@ class RatingProcessorBase(object):
         """
         return self.process(data)
 
+    def nodata(self, begin, end):
+        """Handle billing processing when no data has been collected.
+
+        :param begin: Begin of the period.
+        :param end: End of the period.
+        """
+        pass
+
     @abc.abstractmethod
     def process(self, data):
         """Add rating informations to data
