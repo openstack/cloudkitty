a98d3b99e43594fb5455a8d9e5d0f56bb2f2e300
Aaron-DH <dinghh@awcloud.com>
2015-11-30 14:56:04 +0800

    Load wsgi app(api) with paste.deploy
    More scalable to add a filter
    
    Change-Id: Ib5d3bd16d2bfbafc5a0aa5ef3b8db780e3b97520

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -99,6 +99,7 @@ function configure_cloudkitty {
 
     cp $CLOUDKITTY_DIR$CLOUDKITTY_CONF_DIR/policy.json $CLOUDKITTY_CONF_DIR
     cp $CLOUDKITTY_DIR$CLOUDKITTY_CONF.sample $CLOUDKITTY_CONF
+    cp $CLOUDKITTY_DIR$CLOUDKITTY_CONF_DIR/api_paste.ini $CLOUDKITTY_CONF_DIR
     iniset_rpc_backend cloudkitty $CLOUDKITTY_CONF DEFAULT
 
     iniset $CLOUDKITTY_CONF DEFAULT notification_topics 'notifications'
