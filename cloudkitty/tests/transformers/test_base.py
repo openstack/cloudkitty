9c9d0b6abe1bb0e7352c9092878088f7eebab67e
Stéphane Albert <stephane.albert@objectif-libre.com>
2016-05-10 18:17:00 +0200

    Refactor ceilometer transformer
    Transitioned to new transformer class to have cleaner and smaller code.
    Refactored tests to easily handle multiple transformers tests.
    Modified BaseTransformer to ensure all data is passed to custom
    functions.
    
    Change-Id: I504a4c65c891d283aee93bf821ecd479cbdf1bbb

--- cloudkitty/tests/transformers/test_base.py
+++ cloudkitty/tests/transformers/test_base.py
@@ -17,11 +17,9 @@
 #
 import copy
 
-import six
-
 from cloudkitty import tests
 from cloudkitty.tests import samples
-from cloudkitty import transformer
+from cloudkitty.tests import transformers as t_transformers
 
 TRANS_METADATA = {
     'availability_zone': 'nova',
@@ -32,81 +30,44 @@ TRANS_METADATA = {
     'vcpus': '1'}
 
 
-class Transformer(transformer.BaseTransformer):
-    compute_map = {
-        'name': ['name', 'display_name'],
-        'flavor': ['flavor', 'flavor.name', 'instance_type'],
-        'vcpus': ['vcpus'],
-        'memory': ['memory', 'memory_mb'],
-        'image_id': ['image_id', 'image.id', 'image_meta.base_image_ref'],
-        'availability_zone': [
-            'availability_zone',
-            'OS-EXT-AZ.availability_zone'],
-    }
-    volume_map = {
-        'volume_id': ['volume_id'],
-        'name': ['display_name'],
-        'availability_zone': ['availability_zone'],
-        'size': ['size'],
-    }
-    test_map = {'test': lambda x, y: 'ok'}
-
-    def _strip_network(self, res_metadata):
-        return {'test': 'ok'}
-
-
-class TransformerMeta(Transformer):
-    metadata_item = 'metadata'
-
-
-class EmptyClass(object):
-    pass
-
-
-class ClassWithAttr(object):
-    def __init__(self):
-        for key, val in six.iteritems(samples.COMPUTE_METADATA):
-            setattr(self, key, val)
-
-
 class TransformerBaseTest(tests.TestCase):
     def setUp(self):
         super(TransformerBaseTest, self).setUp()
 
     def test_strip_resource_on_dict(self):
         metadata = copy.deepcopy(samples.COMPUTE_METADATA)
-        t_test = Transformer()
+        t_test = t_transformers.Transformer()
         result = t_test.strip_resource_data('compute', metadata)
         self.assertEqual(TRANS_METADATA, result)
 
     def test_strip_resource_with_no_rules(self):
         metadata = copy.deepcopy(samples.COMPUTE_METADATA)
-        t_test = Transformer()
+        t_test = t_transformers.Transformer()
         result = t_test.strip_resource_data('unknown', metadata)
         self.assertEqual(samples.COMPUTE_METADATA, result)
 
     def test_strip_resource_with_func(self):
         metadata = {'test': 'dummy'}
-        t_test = Transformer()
+        t_test = t_transformers.Transformer()
         result = t_test.strip_resource_data('test', metadata)
         self.assertEqual({'test': 'ok'}, result)
 
     def test_strip_resource_with_stripping_function(self):
         metadata = {}
-        t_test = Transformer()
+        t_test = t_transformers.Transformer()
         result = t_test.strip_resource_data('network', metadata)
         self.assertEqual({'test': 'ok'}, result)
 
     def test_strip_resource_with_subitem(self):
-        test_obj = EmptyClass()
+        test_obj = t_transformers.EmptyClass()
         test_obj.metadata = copy.deepcopy(samples.COMPUTE_METADATA)
-        t_test = TransformerMeta()
+        t_test = t_transformers.TransformerMeta()
         result = t_test.strip_resource_data('compute', test_obj)
         self.assertEqual(TRANS_METADATA, result)
 
     def test_strip_resource_with_attributes(self):
-        test_obj = EmptyClass()
-        test_obj.metadata = ClassWithAttr()
-        t_test = TransformerMeta()
+        test_obj = t_transformers.EmptyClass()
+        test_obj.metadata = t_transformers.ClassWithAttr()
+        t_test = t_transformers.TransformerMeta()
         result = t_test.strip_resource_data('compute', test_obj)
         self.assertEqual(TRANS_METADATA, result)
