66784899d677168ab86f85a90bed74211135dfd5
ritesh.arya <ritesh.arya@nectechnologies.in>
2017-06-30 17:37:16 +0530

    Remove deprecated oslo_messaging.get_transport
    DeprecationWarning:
    Using function/method 'oslo_messaging.transport.get_transport()'
    is deprecated: use get_rpc_transport or get_notification_transport.
    Replace get_transport with get_rpc_transport.
    
    Change-Id: Iea7ed92aa481b5d1c016693a12b623887c4e86bb

--- cloudkitty/messaging.py
+++ cloudkitty/messaging.py
@@ -33,7 +33,7 @@ def get_transport(url=None, optional=False, cache=True):
     transport = TRANSPORTS.get(cache_key)
     if not transport or not cache:
         try:
-            transport = oslo_messaging.get_transport(cfg.CONF, url)
+            transport = oslo_messaging.get_rpc_transport(cfg.CONF, url)
         except (oslo_messaging.InvalidTransportURL,
                 oslo_messaging.DriverLoadFailure):
             if not optional or url:
