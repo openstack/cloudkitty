46f58fdedb7db097e9291baa11bf0661ac7c3a45
Luka Peschke <luka.peschke@objectif-libre.com>
2019-04-17 17:02:14 +0200

    Add missing import to cloudkitty/common/config
    Change-Id: I3b40921cf869f4dd21041a0fa828750c3c653684

--- cloudkitty/common/config.py
+++ cloudkitty/common/config.py
@@ -24,6 +24,7 @@ import cloudkitty.config
 import cloudkitty.fetcher
 import cloudkitty.fetcher.gnocchi
 import cloudkitty.fetcher.keystone
+import cloudkitty.fetcher.prometheus
 import cloudkitty.fetcher.source
 import cloudkitty.orchestrator
 import cloudkitty.service
