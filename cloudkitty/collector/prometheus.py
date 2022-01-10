fe34ef976817f777ddfd49f1ad27925d91305e4a
Pierre Riteau <pierre@stackhpc.com>
2022-01-10 15:20:29 +0100

    Support customising Prometheus queries
    This commit adds support for adding optional prefix and/or suffix to
    Prometheus queries.
    
    For example, this can be used to perform vector matches between the
    collected metric and another one, to gather additional metadata.
    
    Change-Id: I725f0f5ad00b67f55bcacaf8447e050af3815c73

--- cloudkitty/collector/prometheus.py
+++ cloudkitty/collector/prometheus.py
@@ -18,6 +18,7 @@ from decimal import ROUND_HALF_UP
 
 from oslo_config import cfg
 from oslo_log import log
+from voluptuous import All
 from voluptuous import In
 from voluptuous import Optional
 from voluptuous import Required
@@ -83,7 +84,9 @@ PROMETHEUS_EXTRA_SCHEMA = {
                 'changes', 'delta', 'deriv',
                 'idelta', 'irange', 'irate',
                 'rate'
-            ])
+            ]),
+        Optional('query_prefix', default=''): All(str),
+        Optional('query_suffix', default=''): All(str),
     }
 }
 
@@ -159,6 +162,8 @@ class PrometheusCollector(collector.BaseCollector):
             'range_function')
         groupby = self.conf[metric_name].get('groupby', [])
         metadata = self.conf[metric_name].get('metadata', [])
+        query_prefix = self.conf[metric_name]['extra_args']['query_prefix']
+        query_suffix = self.conf[metric_name]['extra_args']['query_suffix']
         period = tzutils.diff_seconds(end, start)
         time = end
 
@@ -197,6 +202,14 @@ class PrometheusCollector(collector.BaseCollector):
             query,
             ', '.join(groupby + metadata)
         )
+
+        # Add custom query prefix
+        if query_prefix:
+            query = "{0} {1}".format(query_prefix, query)
+
+        # Add custom query suffix
+        if query_suffix:
+            query = "{0} {1}".format(query, query_suffix)
 
         try:
             res = self._conn.get_instant(
