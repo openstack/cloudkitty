9c53bc1bc1b39481f7525e29d3627a7c996d36b0
Pierre Riteau <pierre@stackhpc.com>
2023-02-28 17:08:03 +0100

    Fix spacing in log message
    Change-Id: I8ca1f3776fe43ebc7d7ec3c4c912ca7c2d0ea1bc

--- cloudkitty/storage_state/__init__.py
+++ cloudkitty/storage_state/__init__.py
@@ -241,8 +241,8 @@ class StateManager(object):
 
     def get_state(self, identifier,
                   fetcher=None, collector=None, scope_key=None):
-        LOG.warning("The method 'get_state' is deprecated."
-                    "Consider using the new method"
+        LOG.warning("The method 'get_state' is deprecated. "
+                    "Consider using the new method "
                     "'get_last_processed_timestamp'.")
         return self.get_last_processed_timestamp(
             identifier, fetcher, collector, scope_key)
